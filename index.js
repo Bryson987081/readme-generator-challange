cd26ac041bf838f0b595010e7e4bd6e713fcc34d
Bryson <brysob7617@gmail.com>
2024-06-30 12:08:48 -0400

    added basic promppts for the user to answer

--- index.js
+++ index.js
@@ -2,7 +2,59 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter title",
+      default: "README Title",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter description",
+      default: "Description text",
+    },
+    {
+      type: "input",
+      name: "tableOfContents",
+      message: "Enter table of contents",
+      default: "Contents",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation methods",
+      default: "installation",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Enter license",
+      default: "license",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter contribution",
+        default: "contribution",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Enter test",
+        default: "test",
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Enter questions",
+        default: "questions",
+      },
+  ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
