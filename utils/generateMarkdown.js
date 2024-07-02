1e1a8eef67077d816bfd1ed92f5c882c64069d49
Bryson <brysob7617@gmail.com>
2024-07-01 22:03:57 -0400

    added code to provide links to the licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,7 +16,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Apache License 2.0') {
+    return 'https://www.apache.org/licenses/LICENSE-2.0';
+  } else if (license === 'MIT License') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'Eclipse Publlic License 2.0') {
+    return 'https://www.eclipse.org/legal/epl-2.0/';
+  } else if (license === 'Mozilla Public License 2.0') {
+    return 'https://www.mozilla.org/en-US/MPL/2.0/';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
