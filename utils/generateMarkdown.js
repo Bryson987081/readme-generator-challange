44cadc047e8f72e1e9db2cccd79883a429f3957c
Bryson <brysob7617@gmail.com>
2024-07-01 22:00:43 -0400

    added code to render the license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache License 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache-blue.svg)]';
+  } else if (license === 'MIT License') {
+    return '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  } else if (license === 'Eclipse Publlic License 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Eclipse-purple.svg)]';
+  } else if (license === 'Mozilla Public License 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Mozilla-red.svg)]';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
